340813709a703fd7fa6182d6307f190bd3dcfdb0
Reza Duha <rezafauzizisiwaduha@gmail.com>
2023-05-03 23:51:25 +0700

    feat(type interface): create type interface for movie

---
+++ src/types/movie.d.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  Title: string
+  imdbID: string
+  Poster: string
+  Plot?: string
+  imdbRating?: string
+  imdbVotes?: string
+}
---
+++ src/types/movies.d.ts
@@ -0,0 +1,5 @@
+import type { Movie } from "./movie";
+
+export interface Movies {
+  Search: Movie[]
+}

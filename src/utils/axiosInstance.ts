7663a41122ab612918ff5368c2655206fb31e80c
Reza Duha <rezafauzizisiwaduha@gmail.com>
2023-05-03 23:39:27 +0700

    build(library): add axios and create the instance with omdb api

---
+++ src/utils/axiosInstance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  params: {
+    apikey: import.meta.env.VITE_API_KEY
+  }
+})
+
+export default instance
